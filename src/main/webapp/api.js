5e2cccf5d80d47a6bd836c8a957bb068a5f5042d
AJ-DataFI <victor.ajor@thepalladiumgroup.com>
2025-04-29 11:39:55 +0100

    Update api.js

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,4 +4,4 @@
 //export const url = "http://localhost:8380/api/v1/";
 ////export const apiUrl = "http://localhost:9090/api/";
 //export const token =
-//  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ1OTYwNDMwfQ._oS1T32j4OD5EZJViowOu7bhJxOmQTuGJatU0N7NIEguCRaLWq1OVs32CSEEy4O9PSfazFo4W99Xx722dgg2Cg";
+//  "";
