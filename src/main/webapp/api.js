e30618f8df0c2f253bf5e7d0a5c1187ba878128e
AJ-DataFI <victor.ajor@thepalladiumgroup.com>
2025-05-19 16:07:23 +0100

    fix: sorting samples dates optimized

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,4 +4,4 @@ export const url = "/api/v1/";
 // export const url = "http://localhost:8388/api/v1/";
 // //export const apiUrl = "http://localhost:9090/api/";
 // export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ3NjE0NjAyfQ.Z7r8b60VDkDe0q1bttrLTMMsLClUpgNsCMmAAVR5Pl8FxL0I6MQ2gH4yWLizUkI_9NNyGW2T-ZMBi1FXKWL4Dg";
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ3NzA4NTQyfQ.VP3WA_XrV8oeqd5WkqPthtZdyo-7YMQ6qGChWtkcJVXdv8vHtrmTPseAr9e5pKiPjNIDOgzIhlYJbNTk6LhMKQ";
